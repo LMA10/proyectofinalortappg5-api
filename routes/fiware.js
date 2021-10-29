2b3779ff7b310d16ddd76e6be484f5e2965c95df
FedericoPupkiewicz <federicopupkiewicz@gmail.com>
2021-10-29 19:39:57 -0300

    POST entities

--- routes/fiware.js
+++ routes/fiware.js
@@ -1,13 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
 var express = require("express");
 var router = express.Router();
 const auth = require("../middleware/auth");
 const NGSI = require("ngsijs");
-const conVM = process.env.URL_VM_ORION
+
+
+// const conVM = process.env.URL_VM_ORION
+const conVM = process.env.URL_LOCAL_ORION //LOCAL
 const connection = new NGSI.Connection(`${conVM}`);
 
 router.get("/entities", auth, async (req, res, next) => {
   let arr = [];
-  await connection.v2.listEntities().then((response) => {
+  await connection.v2.listEntities({limit:100}).then((response) => {
     response.results.forEach((entity) => {
       arr.push(entity);
     });
@@ -17,15 +21,15 @@ router.get("/entities", auth, async (req, res, next) => {
 
 router.get("/entities/:id", auth, async (req, res) => {
   await connection.v2.getEntity(req.params.id).then((response) => {
-      res.status(200).send(response)
+    res.status(200).send(response)
   });
 });
 
 
 router.get("/entities/type/:type", auth, async (req, res) => {
   let arr = [];
-  let type=req.params.type
-  await connection.v2.listEntities({type:type}).then((response) => {
+  let type = req.params.type
+  await connection.v2.listEntities({ type: type }).then((response) => {
     response.results.forEach((entity) => {
       arr.push(entity);
     });
@@ -34,8 +38,8 @@ router.get("/entities/type/:type", auth, async (req, res) => {
 });
 router.get("/entities/query/:query", auth, async (req, res) => {
   let arr = [];
-  let query=req.params.query
-  await connection.v2.listEntities({q:query}).then((response) => {
+  let query = req.params.query
+  await connection.v2.listEntities({ q: query }).then((response) => {
     response.results.forEach((entity) => {
       arr.push(entity);
     });
@@ -43,6 +47,50 @@ router.get("/entities/query/:query", auth, async (req, res) => {
   res.send(arr);
 });
 
+
+router.post("/entities/add/add", auth, async (req, res) => {
+
+  let id = uuidv4()
+  let body = {
+    "id": "urn:ngsi-ld:" + req.body.type + ":" + id,
+    "type": req.body.type,
+    "name": {
+      "type": "Text",
+      "value": req.body.name
+    }
+  }
+  if (req.body.type == "Eje") {
+    body.refMunicipio = {
+      "type": "Relationship",
+      "value": req.body.id,
+    }}
+ else if(req.body.type == "SubEje") {
+  body.refEje = {
+    "type": "Relationship",
+    "value": req.body.id,
+  }  
+ }
+ else if(req.body.type == "Indicator") {
+   body.indicatorType ={
+     "type" : "Text",
+     "value" : req.body.tipoDato
+   }
+   body.description ={
+     "type" : "Text",
+     "value" : req.body.descripcion
+   }
+  body.refSubEje = {
+    "type": "Relationship",
+    "value": req.body.id,
+  }
+  
+}
+  await connection.v2.createEntity(body).then((response) => {
+    res.status(200).send(response.entity);
+  })
+
+
+});
 
 
 module.exports = router;
