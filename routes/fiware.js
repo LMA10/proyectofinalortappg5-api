959166a5d9e5541c47e62ce85c97c26832b8cad0
unknown <federicobecker97@hotmail.com>
2021-11-02 21:38:27 -0300

    Carga de metas

--- routes/fiware.js
+++ routes/fiware.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 const auth = require("../middleware/auth");
 const NGSI = require("ngsijs");
-const conVM = process.env.URL_VM_ORION
+const { response } = require("express");
+const moment = require("moment");
+const conVM = process.env.URL_LOCAL_ORION
 const connection = new NGSI.Connection(`${conVM}`);
 
 router.get("/entities", auth, async (req, res, next) => {
@@ -17,15 +19,15 @@ router.get("/entities", auth, async (req, res, next) => {
 
 router.get("/entities/:id", auth, async (req, res) => {
   await connection.v2.getEntity(req.params.id).then((response) => {
-      res.status(200).send(response)
+    res.status(200).send(response)
   });
 });
 
 
 router.get("/entities/type/:type", auth, async (req, res) => {
   let arr = [];
-  let type=req.params.type
-  await connection.v2.listEntities({type:type}).then((response) => {
+  let type = req.params.type
+  await connection.v2.listEntities({ type: type }).then((response) => {
     response.results.forEach((entity) => {
       arr.push(entity);
     });
@@ -34,13 +36,30 @@ router.get("/entities/type/:type", auth, async (req, res) => {
 });
 router.get("/entities/query/:query", auth, async (req, res) => {
   let arr = [];
-  let query=req.params.query
-  await connection.v2.listEntities({q:query}).then((response) => {
+  let query = req.params.query
+  await connection.v2.listEntities({ q: query }).then((response) => {
     response.results.forEach((entity) => {
       arr.push(entity);
     });
   });
   res.send(arr);
+});
+
+
+router.put("/entities/add/goal", auth, async (req, res) => {
+  await connection.v2.updateEntityAttributes({
+    "id":req.body.id,
+    "goal":{
+      "type":"Integer",
+      "value":req.body.monto,
+    },
+    "goalDate":{
+      "type":"DateTime",
+      "value":moment((req.body.fecha)).format('YYYY-MM-DDThh:mm:ss.ssZ')
+    }
+  }).then((response)=>{
+    res.status(200).send(response)
+  })
 });
 
 
